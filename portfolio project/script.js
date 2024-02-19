ed8d1676a9472cd16024f346e5ae6732e3782f77
iamindra7 <indra69alt@gmail.com>
2024-02-19 18:36:03 +0530

    added color themes changing options

--- portfolio project/script.js
+++ portfolio project/script.js
@@ -7,6 +7,34 @@ const home = document.querySelector(".home-menu");
 const about = document.querySelector(".about-menu");
 const portfolio = document.querySelector(".portfolio-menu");
 const contact = document.querySelector(".contact-menu");
+const color = document.querySelector(":root");
+// var colorstyle = getComputedStyle(color);
+const green = document.querySelector(".green");
+const red = document.querySelector(".red");
+const yellow = document.querySelector(".yellow");
+const pink = document.querySelector(".pink");
+const purple = document.querySelector(".purple");
+
+const grfnc = function () {
+  color.style.setProperty("--primary-color", "green");
+  color.style.setProperty("--secondary-color", "#56fc03");
+};
+const rdfnc = function () {
+  color.style.setProperty("--primary-color", "red");
+  color.style.setProperty("--secondary-color", "#ee3131");
+};
+const ylfnc = function () {
+  color.style.setProperty("--primary-color", "yellow");
+  color.style.setProperty("--secondary-color", "#ede655");
+};
+const pnkfnc = function () {
+  color.style.setProperty("--primary-color", "pink");
+  color.style.setProperty("--secondary-color", "##e3a3cc");
+};
+const prplfnc = function () {
+  color.style.setProperty("--primary-color", "purple");
+  color.style.setProperty("--secondary-color", "#ad4ef5");
+};
 
 const showhome = function () {
   homepage.classList.remove("hidden");
@@ -53,3 +81,8 @@ home.addEventListener("click", showhome);
 about.addEventListener("click", showabout);
 portfolio.addEventListener("click", showpof);
 contact.addEventListener("click", showcont);
+green.addEventListener("click", grfnc);
+red.addEventListener("click", rdfnc);
+yellow.addEventListener("click", ylfnc);
+pink.addEventListener("click", pnkfnc);
+purple.addEventListener("click", prplfnc);
